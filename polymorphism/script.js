a69508115d326c842d06a3ef3ee69ea3741ea68d
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2022-12-07 13:08:02 +0300

    add new functions to polymorphism file

--- polymorphism/script.js
+++ polymorphism/script.js
@@ -5,30 +5,72 @@ class Shape
 
     }
 }
-class Circle extends Shape
+class Triangle extends Shape
 {
-    constructor(r)
-    {
-        super()
-        this.radius=r
-    }
-    area()
-    {
-        return Math.PI*this.radius**2;
-    }
-}
-class Rectangle extends Shape
-{
-    constructor(w,h)
+    constructor(b,h)
     {
         super();
-        this.width=w;
+        this.base=b;
         this.height=h;
     }
     area()
     {
-        return this.width*this.height;
+        return 0.5*this.base*this.height;
     }
 }
-const shapes=[new Circle(2),new Rectangle(2,3),new Circle(7)];
+class Parallelogram extends Shape
+{
+    constructor(m,n)
+    {
+        super();
+        this.base=m;
+        this.height=n;
+    }
+    area()
+    {
+        return this.base*this.height;
+    }
+}
+class Trapezium extends Shape
+{
+    constructor(k,l,o)
+    {
+        super();
+        this.base1=k;
+        this.base2=l;
+        this.height=o;
+
+    }
+    area()
+    {
+        return 0.5*(this.base1+this.base2)*this.height;
+    }
+}
+// class Circle extends Shape
+// {
+//     constructor(r)
+//     {
+//         super()
+//         this.radius=r
+//     }
+//     area()
+//     {
+//         return Math.PI*this.radius**2;
+//     }
+// }
+// class Rectangle extends Shape
+// {
+//     constructor(w,h)
+//     {
+//         super();
+//         this.width=w;
+//         this.height=h;
+//     }
+//     area()
+//     {
+//         return this.width*this.height;
+//     }
+// }
+//const shapes=[new Circle(2),new Rectangle(2,3),new Circle(7)];
+const shapes=[new Triangle(10,20),new Parallelogram(10,20),new Trapezium(10,20,30)];
 shapes.forEach(item=>console.log(item.area()));
