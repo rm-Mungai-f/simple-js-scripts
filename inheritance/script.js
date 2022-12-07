43bdfbb836d80c4a7790229bfce54d1e1fc9142a
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2022-12-07 11:58:57 +0300

    js script to demonstrate inheritance

---
+++ inheritance/script.js
@@ -0,0 +1,36 @@
+class ClassA {
+    constructor() {
+        this.propA = 'A';
+    }
+
+    methodA() {
+        return this.propA;
+    }
+}
+
+class ClassB extends ClassA {
+    constructor() {
+        super();
+        this.propB = 'B';
+    }
+
+    methodA() {
+        return 'NEW B';
+    }
+
+    methodB() {
+        return this.propB + this.methodA();
+    }
+}
+
+class ClassC extends ClassB {
+    constructor() {
+        super();
+        this.propC = 'C';
+         this.testProp = 1;
+    }
+
+    methodC() {
+        return this.propC + super.methodB();
+    }
+}
