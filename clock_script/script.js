a4650431a6b6a0f27c190222932d0c083b404193
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-11 12:21:31 +0300

    create a sort array for a list of array items

--- clock_script/script.js
+++ clock_script/script.js
@@ -4,18 +4,18 @@ function my_Clock()
     this.hours=this.cur_date.getHours();
     this.minutes=this.cur_date.getMinutes();
     this.seconds=this.cur_date.getSeconds();
-}
+};
 
 my_Clock.prototype.run=function()
 {
     setInterval(this.update.bind(this),1000);
-}
+};
 
 my_Clock.prototype.update=function()
 {
     this.updateTime(1);
     console.log(this.hours+":"+this.minutes+":"+this.seconds)
-}
+};
 
 my_Clock.prototype.updateTime=function(secs)
 {
